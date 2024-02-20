5736eb3421a0cc0ab9bda2c51e60a91d1a295151
Maria <karpen.masha@gmail.com>
2024-02-20 10:04:52 +0300

    Решение задачи Счастливый билет

---
+++ happy_ticket.py
@@ -0,0 +1,28 @@
+value = input ('Введите номер билета, состоящий из цифр ')
+print ('Ваш билет: '+ value)
+lenth_value = len (value)
+cond_1 = lenth_value % 2  #проверка условия 1: четное количество символов
+cond_2 = value.isdigit() #проверка условия 2: строка содержит только числовые символы
+
+if cond_1 == 0 and cond_2:
+    middle = lenth_value / 2 #промежуточная переменная
+    i = 0
+    summ_1 = int (value [i]) #сумма первой половины цифр
+    while i < (middle - 1):
+        i = i + 1
+        summ_1 = int (value [i]) + summ_1
+    print (summ_1) #для проверки вывод суммы
+
+    summ_2 = 0 #сумма второй половины цифр
+    while i < (lenth_value - 1):
+        i = i + 1
+        summ_2 = int (value [i]) + summ_2
+    print (summ_2) #для проверки вывод суммы
+
+    if summ_1 == summ_2:
+        print ('Ваш билет счастливый!')
+    else: print ('Билет не счастливый')
+
+else:
+    print ('Условия задачи не выполнены')
+
